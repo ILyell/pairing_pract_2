5f6e40f2376bd9af08994e235f6b2a3afaaa7788
Ian Lyell <ian.lyell@yahoo.com>
2023-03-29 12:27:49 -0500

    Add 'or' conditional

--- plants.rb
+++ plants.rb
@@ -1,8 +1,10 @@
 gets_sunshine = true
-ounces_of_water = 11
+ounces_of_water = 9
 
 if gets_sunshine == true && ounces_of_water >= 10
     p "This plant will thrive"
+elsif gets_sunshine == true || ounces_of_water >= 10
+    p "Becareful your plant is in danger!"
 else
     p "This plant will die!!!"
 end
